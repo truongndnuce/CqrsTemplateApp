eb281f55af70f70637e4a7e61c112dd752c165c2
DanTruong0807 <96624214+truongndnuce@users.noreply.github.com>
2026-06-05 00:27:45 +0700

    config serilog

--- src/CqrsApp.API/Program.cs
+++ src/CqrsApp.API/Program.cs
@@ -1,30 +1,55 @@
 using CqrsApp.Application.DependencyInjection.Extentions;
 using CqrsApp.Persistence.DependencyInjection.Extentions;
 using CqrsApp.Persistence.DependencyInjection.Options;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddControllers()
-    .AddApplicationPart(CqrsApp.Presentation.AssemblyReferences.Assembly);
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddConfigureMediatR();
-builder.Services.ConfigureSqlServerRetryOptions(builder.Configuration.GetSection(nameof(SqlServerRetryOptions)));
-builder.Services.AddSqlConfiguration();
-builder.Services.AddConfigurationAutoMapper();
-builder.Services.AddRepositoryBaseConfiguration();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Starting application...");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, services, configuration) =>
+        configuration.ReadFrom.Configuration(context.Configuration)
+                     .ReadFrom.Services(services)
+                     .Enrich.FromLogContext());
+
+    builder.Services.AddControllers()
+        .AddApplicationPart(CqrsApp.Presentation.AssemblyReferences.Assembly);
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddConfigureMediatR();
+    builder.Services.ConfigureSqlServerRetryOptions(builder.Configuration.GetSection(nameof(SqlServerRetryOptions)));
+    builder.Services.AddSqlConfiguration();
+    builder.Services.AddConfigurationAutoMapper();
+    builder.Services.AddRepositoryBaseConfiguration();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.MapControllers();
-
-app.Run();
+catch (Exception ex) when (ex is not HostAbortedException)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
